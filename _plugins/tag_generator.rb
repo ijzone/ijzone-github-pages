690c4ad16d306410f070a371fb09c187aed3a442
ikjoo <ijzone@dejay.co.kr>
2024-08-28 17:36:34 +0900

    chore: 태그 페이지 - post 태그 거는 페이지 추가

---
+++ _plugins/tag_generator.rb
@@ -0,0 +1,16 @@
+Jekyll::Hooks.register :site, :post_write do |site|
+    tags = site.tags.keys
+    tags.each do |tag|
+      tag_dir = File.join(site.source, 'tags', tag)
+      Dir.mkdir(tag_dir) unless File.exists?(tag_dir)
+      tag_page = File.join(tag_dir, 'index.md')
+      
+      File.open(tag_page, 'w') do |f|
+        f.write("---\n")
+        f.write("layout: tag\n")
+        f.write("tag: #{tag}\n")
+        f.write("---\n")
+      end
+    end
+  end
+  
